4dadc29e53738599cf52b198fa6fe4d84ec77785
OldSerpskiStalker <ictopiar@gmail.com>
2023-12-24 17:03:56 +0500

    * Added test console for log

---
+++ StartLog/StartLog.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+#include <iostream>
+#include <fstream>
+#include <span>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+#include "..\MultiLogManager\Exports.hpp"
+#include "..\MultiLogManager\Log\Log.hpp"
+
+using namespace Log;
+
+int main()
+{
+    pLog = new CLog();
+    std::cout << "Hello World!\n";
+
+    pLog->CreateFileSettings();
+
+    delete pLog;
+
+    return 1;
+}
